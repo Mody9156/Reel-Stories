16bff01ff4d18c76ecbe8589e13f0a91f3f206cc
KEITA Modibo <Modykeita2@gmail.com>
2025-11-03 15:58:16 +0100

    nouveau dossier

---
+++ Reel&Stories/Reel_StoriesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Reel_StoriesApp.swift
+//  Reel&Stories
+//
+//  Created by Modibo on 03/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct Reel_StoriesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
